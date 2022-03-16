edefc5b16b85a745029afa7be4ab829b2ecee45d
Evan Gonzalez <evangonzalez.mail@gmail.com>
2022-03-16 15:42:36 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function() {
+  const d = new Date();
+  const hours = d.getHours();
+  const night = hours >= 19 || hours <= 7; // between 7pm and 7am
+  const body = document.querySelector('body');
+  const toggle = document.getElementById('toggle');
+  const input = document.getElementById('switch');
+
+  if (night) {
+    input.checked = true;
+    body.classList.add('night');
+  }
+
+  toggle.addEventListener('click', function() {
+    const isChecked = input.checked;
+    if (isChecked) {
+      body.classList.remove('night');
+    } else {
+      body.classList.add('night');
+    }
+  });
+
+  const introHeight = document.querySelector('.intro').offsetHeight;
+  const topButton = document.getElementById('top-button');
+  const $topButton = $('#top-button');
+
+  window.addEventListener(
+    'scroll',
+    function() {
+      if (window.scrollY > introHeight) {
+        $topButton.fadeIn();
+      } else {
+        $topButton.fadeOut();
+      }
+    },
+    false
+  );
+
+  topButton.addEventListener('click', function() {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+  });
+
+  window.sr = ScrollReveal({
+    reset: false,
+    duration: 600,
+    easing: 'cubic-bezier(.694,0,.335,1)',
+    scale: 1,
+    viewFactor: 0.3,
+  });
+
+  sr.reveal('.background');
+  sr.reveal('.skills');
+  sr.reveal('.experience', { viewFactor: 0.15 });
+});
